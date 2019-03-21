70e4644fed9d8499f8495df87cd935aee7dea763
Everett Grassler <dragwar9@gmail.com>
2019-03-21 14:45:15 -0500

    Trying out switch labels

--- PlayingCardsDeck/Program.cs
+++ PlayingCardsDeck/Program.cs
@@ -9,8 +9,12 @@ namespace PlayingCardsDeck
             Console.Title = "Deck consists of: (52 total playing cards), (4 suits), (13 playing cards per suit)";
 
 
+
+        Start:
+            
             DeckManager deckManager = new DeckManager();
 
+            Console.WriteLine("Deck: ");
             foreach (PlayingCard card in deckManager.Deck)
             {
                 Console.WriteLine(card.ToString());
@@ -38,8 +42,27 @@ namespace PlayingCardsDeck
             deckManager.DisplayDeckInfo();
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to exit . . . ");
-            Console.ReadKey(false);
+
+
+        LeavingMenu:
+
+            Console.WriteLine("\nPress 1 to Restart Test");
+            Console.WriteLine("Press 2 to Exit");
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Console.WriteLine("Restarting . . .\n");
+                    goto Start;
+
+                case ConsoleKey.D2:
+                    break;
+
+                default:
+                    goto LeavingMenu;
+            }
         }
     }
 }
